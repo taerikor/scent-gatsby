69f48b6477d28c0c83042cfce8f38ba0426bef9e
taerikor <akdud153@gmail.com>
2022-02-14 21:42:12 +0900

    Add gatsby image plugin, fix css

--- src/components/header.js
+++ src/components/header.js
@@ -2,26 +2,36 @@ import { Link } from "gatsby"
 import React from "react"
 
 import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Logo = "../assets/img/logo.png"
 
 const HeaderContainer = styled.div`
-  /* width: 100%;
-  height: 50px; */
   display: flex;
   flex-direction: column;
   position: absolute;
   top: 0;
   left: 0;
+  padding: 20px;
+  color: var(--primary-color);
+  font-size: 1rem;
 
   & a {
     all: unset;
     cursor: pointer;
+    margin-bottom: 8px;
+  }
+  & a:hover {
+    color: var(--hover-color);
   }
 `
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <Link to="/">LOGO</Link>
+      <Link to="/">
+        <StaticImage src={Logo} alt="logo" placeholder="blurred" width={150} />
+      </Link>
       <Link to="/discography">discography</Link>
       <Link to="/biography">biography</Link>
       <Link to="/video">video</Link>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,9 @@
 import React from "react"
-import { createGlobalStyle } from "styled-components"
 import Header from "./header"
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin:0;
-    padding: 100px;
-    background-color: black;
-    color: white;
-  }
-`
 
 const Layout = ({ children }) => {
   return (
     <>
-      <GlobalStyle />
       <Header />
       {children}
     </>
