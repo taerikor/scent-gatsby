7909d9ae0243b76886101bc7f9a7a1d59c212125
taerikor <akdud153@gmail.com>
2022-02-20 18:45:25 +0900

    Add album slider in discography page

---
+++ src/components/slider/index.js
@@ -0,0 +1,83 @@
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import styled from "styled-components"
+
+import "swiper/css"
+import "swiper/css/effect-coverflow"
+
+import "./styles.css"
+
+import { EffectCoverflow } from "swiper"
+import { StaticImage } from "gatsby-plugin-image"
+
+const CardWrapper = styled.div`
+  text-align: center;
+`
+const StyledLink = styled.a`
+  color: white;
+  &:hover {
+    text-decoration: underline 0.1em rgba(255, 255, 255, 0);
+    transition: text-decoration-color 300ms;
+  }
+`
+const Slider = () => {
+  return (
+    <>
+      <Swiper
+        effect={"coverflow"}
+        centeredSlides={true}
+        slidesPerView={3}
+        loop={true}
+        spaceBetween={100}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <CardWrapper>
+            <StyledLink
+              href="https://linktr.ee/scentofficial"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StaticImage
+                src={"../../assets/img/fwiw.jpg"}
+                alt="logo"
+                placeholder="blurred"
+                width={400}
+                height={400}
+              />
+              <p>fwiw(for what I whine)</p>
+            </StyledLink>
+          </CardWrapper>
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardWrapper>
+            <StyledLink
+              href="https://linktr.ee/scentofficial"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StaticImage
+                src={"../../assets/img/twa.jpeg"}
+                alt="logo"
+                placeholder="blurred"
+                width={400}
+                height={400}
+              />
+
+              <p>Thus, We Are</p>
+            </StyledLink>
+          </CardWrapper>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  )
+}
+export default Slider
--- src/pages/discography/index.js
+++ src/pages/discography/index.js
@@ -1,10 +1,22 @@
 import React from "react"
+import styled from "styled-components"
 import Layout from "../../components/layout"
+import Slider from "../../components/slider"
+
+const SliderWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 const discography = () => {
   return (
     <Layout>
-      <div>discography</div>
+      <SliderWrapper>
+        <Slider />
+      </SliderWrapper>
     </Layout>
   )
 }
